7e4f145a58ea4ebcaaa353f381ccb9ee6254677a
RajkumarKundu1992 <kundurajkumar1992@gmail.com>
2019-07-22 19:48:01 +0530

    reverse a input string

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World---By Rajkumar
-    <button onClick={function () { alert("Msg saved") }}>Add</button ><br />
-      <button onClick={function () { alert("Msg saved") }} > Sub</button>
+      Hello World---By Rajkumar<br /><br />
+      <button >Add</button ><br />
+      <label id="num">press (+ or -)</label><br />
+      <button onClick={function () { alert("Msg saved") }} > Sub</button><br /><br />
+      <input id="txt" type="text" name="text" placeholder="enter text" onChange={reverseInput} />
+
+      <label id="lbl"> hi</label>
     </div >
+
   );
 }
+
+function handleClick() {
+  alert("msg saved")
+}
+
+
+function reverseInput(r) {
+  var a = document.getElementById("txt").value;
+  // document.getElementById("lbl").textContent = a.split('').reverse().join('');
+  var o = '';
+  for (var i = a.length - 1; i >= 0; i--)
+    o += a[i];
+
+  document.getElementById("lbl").textContent = o;
+}
+
+
 
 export default App;
